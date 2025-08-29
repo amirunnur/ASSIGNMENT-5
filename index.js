35271c375e169f042b9f79c4fac415c20b1d5b53
amirunnur <amirunnur88@gmail.com>
2025-08-29 19:12:29 +0600

    second commit

---
+++ index.js
@@ -0,0 +1,116 @@
+// // heart count
+// let heartCount = 0;
+// const heartCountEl = document.getElementById('heart-count')
+// document.getElementById('heart1').addEventListener('click',function(){
+    
+//     heartCount++;
+//     heartCountEl.textContent = String('heartCount') 
+//     console.log(heartCount)
+
+// })
+
+let coinCount = 100;
+const coinCountEl1 = document.getElementById('coin-count')
+document.getElementById('call1').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl1.textContent = coinCount;
+    alert('Calling National Emergency - 999')
+    
+})
+
+const coinCountEl2 = document.getElementById('coin-count')
+document.getElementById('call2').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl2.textContent = coinCount;
+    alert('Calling to Police Helpline - 999')
+    
+})
+const coinCountEl3 = document.getElementById('coin-count')
+document.getElementById('call3').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl3.textContent = coinCount;
+    alert('Calling for Fire Service - 999')
+    
+})
+const coinCountEl4 = document.getElementById('coin-count')
+document.getElementById('call4').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl4.textContent = coinCount;
+    alert('Calling for Ambulance - 1994-999999')
+    
+})
+const coinCountEl5 = document.getElementById('coin-count')
+document.getElementById('call5').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl5.textContent = coinCount;
+    alert('Calling to Women and child helpline - 109')
+    
+})
+const coinCountEl6 = document.getElementById('coin-count')
+document.getElementById('call6').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl6.textContent = coinCount;
+    alert('Calling to Anti-corruption helpline - 106')
+    
+})
+const coinCountEl7 = document.getElementById('coin-count')
+document.getElementById('call7').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl7.textContent = coinCount;
+    alert('Calling to Electricity Helpline - 16276')
+    
+})
+const coinCountEl8 = document.getElementById('coin-count')
+document.getElementById('call8').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl8.textContent = coinCount;
+    alert('Calling to Brac helpline - 16445')
+    
+})
+const coinCountEl9 = document.getElementById('coin-count')
+document.getElementById('call1').addEventListener('click',function(){
+if(coinCount<20){
+        alert('Not enough coin to call!');
+        return ;
+    }
+    coinCount-=20;
+    coinCountEl9.textContent = coinCount;
+    alert('Calling to Bangladesh railway helpline - i63')
+    
+})
+
+    
+    
+// call history
